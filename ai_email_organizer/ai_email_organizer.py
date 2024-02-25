6ce235d61dbf6f25b5562c006fc735cc050694e1
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-02-25 16:56:23 -0600

    chore: removed the email function and placed in its own file

--- ai_email_organizer/ai_email_organizer.py
+++ ai_email_organizer/ai_email_organizer.py
@@ -1,63 +1,11 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
-
-import os.path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from rxconfig import config
 import reflex as rx
+import email
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction"
+
 filename = f"{config.app_name}/{config.app_name}.py"
-
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
-
-
-def email():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=50000)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-
-    try:
-        # Call the Gmail API
-        service = build("gmail", "v1", credentials=creds)
-        message_list = service.users().messages().list(userId="me").execute()
-        messages = message_list.get("messages", [])
-
-        if not messages:
-            print("No messages found.")
-            return
-        print("Messages:")
-        for message in messages:
-            return_message_snippet = (
-                service.users().messages().get(userId="me", id=message["id"]).execute()
-            )
-            print(return_message_snippet["snippet"])
-            print(return_message_snippet["payload"]["body"].get("data", 0))
-
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f"An error occurred: {error}")
 
 
 class State(rx.State):
