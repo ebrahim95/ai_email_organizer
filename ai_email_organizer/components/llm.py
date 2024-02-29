37b254ec69697ff7b79db5d7fa0eb8ed2da35b99
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-02-29 16:02:28 -0600

    feat: added langchain config

---
+++ ai_email_organizer/components/llm.py
@@ -0,0 +1,32 @@
+from langchain_community.llms import Ollama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+# make i do a chain selection
+output_parser = StrOutputParser()
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are an expert summarizer.  When you recieve the information you will do your best to combine the information given into short and sweet summary ",
+        ),
+        ("user", "{input}"),
+    ]
+)
+
+llm = Ollama(model="phi")
+chain = prompt | llm | output_parser
+
+chain.invoke(
+    {"input": "Summarize the emails. The summarizations need to be in done categories"}
+)
+
+embeddings = OllamaEmbeddings()
+text_splitter = RecursiveCharacterTextSplitter()
+gmail_data = ""
+gmail_split = text_splitter.split_documents(gmail_data)
+vector = FAISS.from_documents(gmail_split, embeddings)
