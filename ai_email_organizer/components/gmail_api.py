0ce43e12b3ad90a42a904b3391b26db15a72ed9b
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-04 17:50:13 -0600

    feat: imported state

--- ai_email_organizer/components/gmail_api.py
+++ ai_email_organizer/components/gmail_api.py
@@ -5,6 +5,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from ..state.gmail_api import gmail_message
 
 
 # If modifying these scopes, delete the file token.json.
