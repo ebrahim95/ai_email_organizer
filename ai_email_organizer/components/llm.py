3067221bbe77be56fb4606c8a0545db6f02450b5
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-06 00:29:14 -0600

    fix: fixed the document creation issues
    need to fix the embeddings issue for microsoft phi model

--- ai_email_organizer/components/llm.py
+++ ai_email_organizer/components/llm.py
@@ -4,33 +4,35 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from .components.gmail_api import email
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from .gmail_api import email
 
 
-output_parser = StrOutputParser()
+def llm():
+    # helps to convert output into string
+    output_parser = StrOutputParser()
 
-prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+    prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
 
-<context>
-{context}
-</context>
+    <context>
+    {context}
+    </context>
 
-Question: {input}""")
+    Question: {input}""")
 
-llm = Ollama(model="phi")
-document_chain = create_stuff_documents_chain(llm, prompt)
+    llm = Ollama(model="phi")
+    document_chain = create_stuff_documents_chain(llm, prompt)
 
-embeddings = OllamaEmbeddings()
-text_splitter = RecursiveCharacterTextSplitter()
-gmail_data = email()
-gmail_split = text_splitter.split_documents(gmail_data)
-vector = FAISS.from_documents(gmail_split, embeddings)
+    gmail_data = email()
+    embeddings = OllamaEmbeddings()
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.create_documents(gmail_data[0])
+    gmail_split = text_splitter.split_documents(documents)
+    vector = FAISS.from_documents(gmail_split, embeddings)
 
+    retriever = vector.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-retriever = vector.as_retriever()
-retrieval_chain = create_retrieval_chain(retriever, document_chain)
-
-response = retrieval_chain.invoke({"input": "Summarize the emails"})
-print(response["answer"])
+    response = retrieval_chain.invoke({"input": "Summarize the emails"})
+    print(response["answer"])
