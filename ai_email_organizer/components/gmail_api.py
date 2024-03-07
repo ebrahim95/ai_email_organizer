f0f98bf208e34f2d4c7f386b01cc58fb1c022830
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-07 00:28:55 -0600

    feat: added regex to filter email data

--- ai_email_organizer/components/gmail_api.py
+++ ai_email_organizer/components/gmail_api.py
@@ -1,6 +1,7 @@
 import reflex as rx
 import base64
 import os.path
+import re
 import email as femail
 from pprint import pprint
 from google.auth.transport.requests import Request
@@ -8,7 +9,6 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from ..state.gmail_api import State
 from bs4 import BeautifulSoup
 
 
@@ -55,11 +55,12 @@ def email():
             token.write(creds.to_json())
 
     message_list_append: list[list[str, str, str]] = []
+    message_list_string: str = ""
     try:
         # Call the Gmail API
         service = build("gmail", "v1", credentials=creds)
         message_list = (
-            service.users().messages().list(userId="me", maxResults="10").execute()
+            service.users().messages().list(userId="me", maxResults="2").execute()
         )
         messages = message_list.get("messages", [])
 
@@ -72,6 +73,7 @@ def email():
             return_message_snippet = (
                 service.users().messages().get(userId="me", id=message["id"]).execute()
             )
+
             payload = return_message_snippet["payload"]
             headers = payload["headers"]
             parts = payload.get("parts", 0)
@@ -94,20 +96,33 @@ def email():
                 decoded = BeautifulSoup(
                     base64.urlsafe_b64decode(data.encode("utf-8")).decode("utf-8"),
                     "lxml",
-                ).getText()
+                )
                 who = "From:" + str(sender)
                 subject = "Subject:" + str(subject)
-                # print("From", sender)
-                # print("Subject", subject)
-                # print(decoded)
-                # print(
-                #     "----------------------------------------------------------------------------------"
-                # )
-                message_list_append.append([who, subject, decoded])
-            #     State.store_message([who, subject, decoded])
-            #     State.set_number()
-            # print(State.count)
-        return message_list_append
+
+                for a in decoded.findAll("a"):
+                    a.extract()
+
+                decoded = decoded.getText(" ", strip=True)
+
+                # use regex to filter out strings
+                # delete ( in between )
+                result = re.sub(r"\([^)]*\)", " ", decoded)
+                # delete \n\r
+                result = re.sub(r"\s*[\r\n\xa0\u200c]+\s*", " ", result)
+                # delete media queries
+                result = re.sub(r"@media[^}]+}", " ", result)
+                # delete between {}
+                result = re.sub(r"{[^{}]*}", " ", result)
+                # delete *-
+                result = re.sub(r"[*-]", " ", result)
+
+                message_list_append.append(who)
+                message_list_append.append(subject)
+                message_list_append.append("message: " + result)
+                message_list_string += who + subject + "message: " + result
+        return message_list_string
+
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
         print(f"An error occurred: {error}")
