390fa24241350663c2521683126b4b4315c52a8e
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-07 23:55:25 -0600

    feat: added login function to set creds to state

--- ai_email_organizer/state/gmail_api.py
+++ ai_email_organizer/state/gmail_api.py
@@ -1,9 +1,22 @@
 import reflex as rx
+import base64
+import os.path
+from pprint import pprint
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
 
 class State(rx.State):
     count: int = 0
     logged_in: bool
+    creds = None
 
     # message list
     message_list: list[list[str, str, str]] = []
@@ -16,3 +29,22 @@ class State(rx.State):
 
     def set_boolean(self, status):
         self.logged_in = status
+
+    def login(self):
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists("token.json"):
+            self.creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES
+                )
+                self.creds = flow.run_local_server(port=50000)
+            # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(self.creds.to_json())
