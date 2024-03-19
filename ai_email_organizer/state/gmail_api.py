077236c194e696e6fe74857dfa7d3156acb68729
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-19 15:55:12 -0500

    feat: removed self.creds and replaced it with creds

--- ai_email_organizer/state/gmail_api.py
+++ ai_email_organizer/state/gmail_api.py
@@ -16,7 +16,6 @@ SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 class State(rx.State):
     count: int = 0
     logged_in: bool
-    creds = None
 
     # message list
     message_list: list[list[str, str, str]] = []
@@ -35,16 +34,16 @@ class State(rx.State):
         # created automatically when the authorization flow completes for the first
         # time.
         if os.path.exists("token.json"):
-            self.creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
         # If there are no (valid) credentials available, let the user log in.
-        if not self.creds or not self.creds.valid:
-            if self.creds and self.creds.expired and self.creds.refresh_token:
-                self.creds.refresh(Request())
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
                     "credentials.json", SCOPES
                 )
-                self.creds = flow.run_local_server(port=50000)
+                creds = flow.run_local_server(port=50000)
             # Save the credentials for the next run
             with open("token.json", "w") as token:
-                token.write(self.creds.to_json())
+                token.write(creds.to_json())
