bc4600a8b9a5e2114e217aa1489d19fa3b6987a5
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-20 19:17:52 -0500

    feat: used regex and beautifulsoup to parse messages

--- ai_email_organizer/components/gmail_api.py
+++ ai_email_organizer/components/gmail_api.py
@@ -1,17 +1,27 @@
 import reflex as rx
+import re
 from pprint import pprint
 from googleapiclient.errors import HttpError
 from simplegmail import Gmail
 from simplegmail.query import construct_query
+from bs4 import BeautifulSoup
 
 
-gmail = Gmail()
+def remove_r_n_mine(string):
+    # result = re.sub(r"\s*[\r\n\xa0\u200c]+\s*", " ", string)
+    # return result
+    soup = BeautifulSoup(string, "lxml")
+    regex_filter = re.sub(r"\s*[\r\n\xa0\u200c]+\s*", " ", soup.get_text())
 
+    return regex_filter
+
+
+#
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
 
-def email(creds=""):
+def email():
     gmail = Gmail()
     message_list_append: list[list[str, str, str]] = []
     message_list_string: str = ""
@@ -22,19 +32,20 @@ def email(creds=""):
         }
         messages = gmail.get_messages(query=construct_query(query_params))
 
-        # Print them out!
         for message in messages:
-            message_list_string += "To: " + message.recipient
-            message_list_string += "From: " + message.sender
-            message_list_string += "Subject: " + message.subject
-            message_list_string += "Date: " + message.date
-            message_list_string += "Preview: " + message.snippet
+            message_list_string += "To: " + remove_r_n_mine(message.recipient)
+            message_list_string += "From: " + remove_r_n_mine(message.sender)
+            message_list_string += "Subject: " + remove_r_n_mine(message.subject)
+            message_list_string += "Date: " + remove_r_n_mine(message.date)
+            message_list_string += "Preview: " + remove_r_n_mine(message.snippet)
 
             if message.plain is not None:
-                message_list_string += (
-                    "Message Body: " + message.plain
+                message_list_string += "Message Body: " + remove_r_n_mine(
+                    message.plain
                 )  # or message.html
 
+            # use regex to filter
+            message_list_append.append(message_list_string)
         return message_list_append
 
     except HttpError as error:
